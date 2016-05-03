9a962e984755a7961e05b9ec2d2b0d9e58b3b2e8
Marie <marie.isabey@orange.fr>
2016-05-03 17:16:14 +0200

    Ajout table Epreuve

---
+++ app/controllers/epreuves_controller.rb
@@ -0,0 +1,2 @@
+class EpreuvesController < ApplicationController
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160503145611) do
+ActiveRecord::Schema.define(version: 20160503151315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,16 @@ ActiveRecord::Schema.define(version: 20160503145611) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "epreuves", force: :cascade do |t|
+    t.date     "date"
+    t.string   "titre"
+    t.integer  "matiere_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "epreuves", ["matiere_id"], name: "index_epreuves_on_matiere_id", using: :btree
 
   create_table "etudiants", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -83,6 +93,7 @@ ActiveRecord::Schema.define(version: 20160503145611) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "epreuves", "matieres"
   add_foreign_key "matieres", "enseignants"
   add_foreign_key "notes", "matieres"
   add_foreign_key "notes", "users"
