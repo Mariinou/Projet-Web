5dbad37f25b4dff0e165364ae0aa2be0ae052a9d
Marie <marie.isabey@orange.fr>
2016-05-03 16:35:49 +0200

    Ajout des tables note et matière

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
   end
 
   devise_for :users
+
+  resources :notes
+
+  resources :matieres
   
   root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
