1d5163e20d9b77816552fb33c19b6fd19f4306cd
Marie <marie.isabey@orange.fr>
2016-05-05 14:56:55 +0200

    V1 des accès restreints aux pages

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,5 @@
 class NotesController < ApplicationController
-	before_action :authenticate_user!
+	load_and_authorize_resource
 
 	def new
 		@note = Note.new
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,5 +2,17 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+  	user ||= User.new
+  	if user.kind_of? Administrateur
+  		can :manage, :all
+
+  	else if user.kind_of? Etudiant
+  		can :read, Note
+  	else 
+  		can :create, Note
+  		can :create, Matiere
+  		can :create, Epreuve
+  	end
+  	end
   end
 end
